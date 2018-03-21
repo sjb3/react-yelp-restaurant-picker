561a917dad30085b3d5ba5e4c42543561d46bf75
Sung J Byun <sungjbyun@Sungs-MacBook-Pro-2.local>
2018-03-21 13:56:19 -0700

    yelp api working

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const express = require('express');
 const app = express();
 const YelpApi = require('./yelp_api');
@@ -5,7 +7,19 @@ const YelpApi = require('./yelp_api');
 
 app.use(express.static('public'));
 
+// routes to access Yelp's api endpoint
+// maybe separate file?
+app.get('/yelp/search', (req, res) => {
+  let params = req.query;
+  console.log('query::', req.query);
+
+  YelpApi.search(params)
+    .then(results => {
+      res.json(results)
+    })
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`http://localhost:${PORT}`)
+  console.log(`>>>>> Server running on: http://localhost:${PORT}`)
 });
--- src/server/yelp_api.js
+++ src/server/yelp_api.js
@@ -1,17 +1,47 @@
 'use strict';
 
-const CLIENT_ID =  'f8XXrm3Nv39dcftleVv5wQ';
+// TODO: move the key to env or config file later
 const API_KEY = 'K1yA1HCefBu6GOrarquTAombN4yzc4LiEt5ltUVDnVXVZNk33yFGmPPFC7-7v-RgXl-DtIi-dbOG-yWT1eR1LQsex--KaPwy5AWgFnWWUkR7IaTnkg0SA_6RGXqxWnYx';
 const axios = require('axios');
 
-// const yelp = new Yelp({ id:CLIENT_ID , apiKey: API_KEY });
 
-// node-yelp-api expired, use just fetch or axios instead
-axios.get('https://api.yelp.com/v3/businesses/search?term=Restaurants&location=Seattle',
-  {headers: {'Authorization': `Bearer ${API_KEY}`}})
-  .then( result => {
-  console.log('SUCCESS', result.data);
+// creating abstraction/interface to get data from the YELP
+// hence, the front-end only cares about the restaurant display, not how to get the data
+// Q: how we gonna use the YelpApi then?
+// search({searchString, some parameters, term, location/coordinates})
+//    => get promise/.then ... => .then(console.log(restaurants))
+
+// makeQuery :: Object => String
+function makeQuery(obj) {
+  const result = '?';
+
+  for(let key in obj) {
+    let str = obj[key] && obj[key].replace(/\s/g, '+');
+    if (str) {
+      result += `${key}=${str}&`
+    }
+  }
+  return result.slice(0, -1);
+};
+
+// console.log(makeQuery({term:'Restaurants', location:'Seattle'}));
+function search(options) {
+  let queryString = makeQuery(options);
+
+  return axios.get(`https://api.yelp.com/v3/businesses/search${queryString}`,
+  {headers: {'Authorization': `Bearer ${API_KEY}`}, })
+  .then(result => {
+    return { success: true, data: result.data.businesses }
   })
-  // .catch(err => {
-  //   console.error('????????', err)
-  // })
+  .catch(err => {
+    console.error('????????', err)
+    return { err: true, message: 'Yelp api crashed' };
+  });
+}
+
+// search({term:'Yoga||yoga', location: 'Seattle'||'seattle'})
+//   .then(console.log)
+
+module.export = {
+  search
+};
